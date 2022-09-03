77fe76efb640c8a862624f6c102a8ff54ffb86ef
joselyngm11 <joselyngm.47@gmail.com>
2022-09-03 03:54:26 +0000

    Cambios style (con flex)

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 let counter = 0;
 
-setInterval(() => {
+let interval = setInterval(() => {
   if (counter == 10) {
     changeCard();
     counter = 0;
@@ -15,9 +15,7 @@ setInterval(() => {
 
 function changeCard() {
   function random_item(figura) {
-    let random = figura[Math.floor(Math.random() * figura.length)];
-
-    return random;
+    return figura[Math.floor(Math.random() * figura.length)];
   }
 
   let figures = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
@@ -40,7 +38,6 @@ function changeCard() {
   let figTop = document.getElementById("topFig");
   let figBottom = document.getElementById("bottomFig");
   let figNumber = document.getElementById("numberFig");
-  //let num = Math.floor(Math.random() * 9 + 2);
 
   let actual = random_item(figures);
   let num = random_item(number);
@@ -61,12 +58,6 @@ function changeCard() {
 }
 
 window.onload = () => {
-  //document.querySelector(".card").classList.add("heart");
-  //setTimeout(() => {
-  //document.querySelector(".card").classList.remove("heart");
-  //document.querySelector(".card").classList.add("spade");
-  //}, 2000); // 2 segundos
-
   changeCard();
 };
 
@@ -74,7 +65,7 @@ let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
   changeCard();
   clearInterval(interval);
-  setInterval(() => {
+  interval = setInterval(() => {
     if (counter == 10) {
       changeCard();
       counter = 0;
